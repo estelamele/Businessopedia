a2e334ac41442b4125834acd3a3546fc8d431afc
estelamele <67207855+estelamele@users.noreply.github.com>
2021-07-15 14:36:06 +0200

    g

--- src/components/MainView/Home/Home.js
+++ src/components/MainView/Home/Home.js
@@ -1,10 +1,22 @@
 import React from "react";
+import {Typography} from "@material-ui/core"
+import { makeStyles } from "@material-ui/core/styles";
 import "./home.css";
 
+const useStyles = makeStyles((theme) => ({
+  typographyStyle: {
+    flex: 1
+  }
+}));
+
 export default function Home() {
+  const classes = useStyles();
   return (
     <div className="head">
       <h1>Businessopedia</h1>
+      <Typography className={classes.typographyStyle}>
+        about us
+      </Typography>
     </div>
   );
 }
